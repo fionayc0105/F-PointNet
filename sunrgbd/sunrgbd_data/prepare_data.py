c5cdb8851ce8d6072afa47e496b09baf96ba7bf0
fiona_hung <fionayc0105@gmail.com>
2023-05-10 10:43:42 +0800

    修改檔案名稱為prepare_data, 加入將數據依據比例進行分割的函式

--- sunrgbd/sunrgbd_data/prepare_data.py
+++ sunrgbd/sunrgbd_data/prepare_data.py
@@ -11,22 +11,24 @@ def save(filename, contents):
     f.close()
 
 
-def split_data(start_idx, count, ratio_train, ratio_valid, ratio_test):
-    df = list(range(start_idx, start_idx + count))
-
-    train, test = model_selection.train_test_split(df, test_size=ratio_test)
+# 將數據集依照比例分割為 train/valid/test
+def split_data(idx_list, ratio_train, ratio_valid, ratio_test):
+    train, test = model_selection.train_test_split(idx_list, test_size=ratio_test)
     ratio = ratio_valid/(1-ratio_test)
     train, valid = model_selection.train_test_split(train, test_size=ratio)
     train.sort()
     valid.sort()
     test.sort()
 
-    c_train = ''.join(['%06d' % (x) + "\n" for x in train])
-    c_valid = ''.join(['%06d' % (x) + "\n" for x in valid])
+    c_train = ''.join(['%s' % (x) + "\n" for x in train])
+    c_valid = ''.join(['%s' % (x) + "\n" for x in valid])
     filename_train = os.path.join(SAVE_DIR, "train_data_idx.txt")
     filename_valid = os.path.join(SAVE_DIR, "val_data_idx.txt")
     save(filename_train, c_train)
     save(filename_valid, c_valid)
 
+
 if __name__=='__main__':
-    split_data(1, 10335, 0.8, 0.19, 0.01)
+    idx_list = list(range(1, 1+10335))
+    split_data(idx_list, 0.8, 0.19, 0.01)
+
