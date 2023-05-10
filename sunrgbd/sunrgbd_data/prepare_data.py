06eb48ffb6ebdc25e189c10e0336e99a44c83218
fiona_hung <fionayc0105@gmail.com>
2023-05-10 14:56:48 +0800

    建立只保留選取之類別的label文件

--- sunrgbd/sunrgbd_data/prepare_data.py
+++ sunrgbd/sunrgbd_data/prepare_data.py
@@ -44,11 +44,31 @@ def categorize_data(idx_list, classname):
                 break
     return cate_list
 
+
+def relabel(idx_list, classname):
+    for i in range(len(idx_list)):
+        data_idx = idx_list[i]
+        objects = SUNRGBD_DARASET.get_label_objects(data_idx)
+        label_name = os.path.join(SUNRGBD_DIR, "label_dimension/%06d.txt" % data_idx)
+        relabel_dir = os.path.join(SUNRGBD_DIR, "label_dimension_%s" % classname)
+        if not os.path.exists(relabel_dir):
+            os.makedirs(relabel_dir)
+        relabel_name = os.path.join(relabel_dir, "%06d.txt" % data_idx)
+        lines = [line.rstrip() for line in open(label_name)]
+        for obj_idx in range(len(objects)):
+            obj = objects[obj_idx]
+            if obj.classname == classname:
+                fp = open(relabel_name, "a+")
+                fp.write(lines[obj_idx]+'\n')
+                fp.close()
+
+
 def resample_and_split_data(idx_list, count, ratio_train, ratio_valid, ratio_test):
     np.random.shuffle(idx_list)
     idx_resample = idx_list[0:count]
     split_data(idx_resample, ratio_train, ratio_valid, ratio_test)
     return idx_resample
+
 
 if __name__=='__main__':
     total_list = list(range(1, 1+10335))
@@ -56,3 +76,5 @@ if __name__=='__main__':
     cate_list = categorize_data(total_list, 'chair')
     # 對數據進行採樣, 保留部份數據
     resample_list = resample_and_split_data(cate_list, 600, 0.8, 0.19, 0.01)
+    # 建立新的label,只保留設定的classname
+    relabel(resample_list, 'chair')
