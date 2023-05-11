d1916178f8379455cb45b49dfc343e329d9f2619
fiona_hung <fionayc0105@gmail.com>
2023-05-11 13:52:09 +0800

    加入測試raw data的作法, 並且將檢測的結果畫在image上(3d box on image)

--- sunrgbd/sunrgbd_detection/test_raw_data.py
+++ sunrgbd/sunrgbd_detection/test_raw_data.py
@@ -11,19 +11,21 @@ import sys
 import numpy as np
 import tensorflow as tf
 import pandas as pd
+import cv2
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 ROOT_DIR = os.path.dirname(BASE_DIR)
 sys.path.append(BASE_DIR)  # model
 sys.path.append(os.path.join(ROOT_DIR, 'utils'))
 sys.path.append(os.path.join(BASE_DIR, '../sunrgbd_data'))
+sys.path.append(os.path.join(BASE_DIR, '../../kitti'))
+
 import roi_seg_box3d_dataset
 from roi_seg_box3d_dataset import NUM_CLASS, NUM_SIZE_CLUSTER, NUM_HEADING_BIN
+from utils import load_zipped_pickle, draw_projected_box3d, draw_label, compute_box_3d, rotz, roty, rotx
+from roi_seg_box3d_dataset import class2angle, class2size, get_3d_box
 import sys
 import os
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(os.path.join(BASE_DIR, '../sunrgbd_data'))
 from sunrgbd_data import sunrgbd_object
 
 parser = argparse.ArgumentParser()
@@ -45,6 +47,27 @@ GPU_INDEX = FLAGS.gpu
 NUM_POINT = FLAGS.num_point
 MODEL = importlib.import_module(FLAGS.model)  # import network module
 NUM_CHANNEL = 6
+
+SAVE_DIR = os.path.join(BASE_DIR, 'viz_results')
+SUNRGBD_ROOT = os.path.join(BASE_DIR, '../sunrgbd_data/matlab/SUNRGBDtoolbox/mysunrgbd')
+SUNRGBD_DIR = os.path.join(SUNRGBD_ROOT, 'training')
+IMG_DIR = os.path.join(SUNRGBD_DIR, 'image')
+TEST_DATASET = roi_seg_box3d_dataset.ROISegBoxDataset(npoints=2048, split='val', rotate_to_center=True, overwritten_data_path=FLAGS.data_path, from_rgb_detection=FLAGS.from_rgb_detection, one_hot=True)
+
+#################
+# drawing color
+#################
+COLORS = { 'bed': (226, 126, 37),
+           'table': (91, 91, 255),
+           'sofa': (255, 192, 30),
+           'chair': (25, 163, 255),
+           'toilet': (0, 0, 255),
+           'desk': (182, 188, 8),
+           'dresser': (249, 148, 165),
+           'night_stand': (61, 231, 98),
+           'bookshelf': (64, 128, 0),
+           'bathtub': (64, 0, 64)
+           }
 
 
 def get_model(batch_size, num_point):
@@ -103,8 +126,6 @@ def inference(sess, ops, pc, one_hot_vec, batch_size):
                            0],))  # score that indicates confidence in 3d box prediction (mask logits+heading+size); no confidence for the center...
 
     ep = ops['end_points']
-    print(num_batches)
-
     for i in range(num_batches):
         feed_dict = {ops['pointclouds_pl']: pc[i * batch_size:(i + 1) * batch_size, ...],
                      ops['one_hot_vec_pl']: one_hot_vec[i * batch_size:(i + 1) * batch_size, :],
@@ -167,21 +188,20 @@ def get_frustum_angle(box2d, calib):
     uvdepth[0, 0:2] = box2d_center
     uvdepth[0, 2] = 20  # some random depth
     box2d_center_upright_camera = calib.project_image_to_upright_camerea(uvdepth)
-    print('UVdepth, center in upright camera: ', uvdepth, box2d_center_upright_camera)
     frustum_angle = -1 * np.arctan2(box2d_center_upright_camera[0, 2], box2d_center_upright_camera[
         0, 0])  # angle as to positive x-axis as in the Zoox paper
     return frustum_angle
 
 
-# get point cloud data
-def get_pc_data(data_idx, type_whitelist=['bed', 'table', 'sofa', 'chair', 'toilet', 'desk', 'dresser', 'night_stand',
+def get_batch(data_idx, type_whitelist=['bed', 'table', 'sofa', 'chair', 'toilet', 'desk', 'dresser', 'night_stand',
                                           'bookshelf', 'bathtub']):
-    dataset = sunrgbd_object(
-        '/home/fiona/ws/PointNet/frustum-pointnets/src/sunrgbd/sunrgbd_data/matlab/SUNRGBDtoolbox/mysunrgbd',
-        'training')
+    dataset = sunrgbd_object(SUNRGBD_ROOT, 'training')
     pc_list = []  # point cloud list,  channel number = 6, xyz,rgb in upright depth coord
     onehot_list = []  # angle of 2d box center from pos x-axis (clockwise)
-    print('------------- ', data_idx)
+    rot_angle_list = []
+    type_list = []
+    box2d_list = []
+
     calib = dataset.get_calibration(data_idx)
     objects = dataset.get_label_objects(data_idx)
     pc_upright_depth = dataset.get_depth(data_idx)
@@ -197,14 +217,15 @@ def get_pc_data(data_idx, type_whitelist=['bed', 'table', 'sofa', 'chair', 'toil
         if obj.classname not in type_whitelist: continue
         box2d = obj.box2d
         xmin, ymin, xmax, ymax = box2d
+
         box_fov_inds = (pc_image_coord[:, 0] < xmax) & (pc_image_coord[:, 0] >= xmin) & (
                 pc_image_coord[:, 1] < ymax) & (pc_image_coord[:, 1] >= ymin)
         # step1: get point cloud
         pc_in_box_fov = pc_upright_camera[box_fov_inds, :]
 
-        # step2: calculate the frustum angle
+        # step2: calculate the frustum angle and rotation angle
         frustum_angle = get_frustum_angle(box2d, calib)
-        print('Frustum angle: ', frustum_angle)
+        rot_angle = get_center_view_rot_angle(frustum_angle)
 
         # step3: sub-sample
         num_point = pc_in_box_fov.shape[0]
@@ -225,29 +246,67 @@ def get_pc_data(data_idx, type_whitelist=['bed', 'table', 'sofa', 'chair', 'toil
         pd.DataFrame(data_dum)
         one_hot_vec = data_dum["table"].tolist()
         onehot_list.append(one_hot_vec)
-    return pc_list, onehot_list
+        rot_angle_list.append(rot_angle)
+        type_list.append(obj.classname)
+        box2d_list.append(box2d)
+
+    return pc_list, onehot_list, rot_angle_list, type_list, box2d_list,  calib
 
 
-def test_pointcloud():
+def test_data(viz=True):
     batch_size = 1
     num_batches = 3
     cur_batch_size = 1
     batch_data_to_feed = np.zeros((batch_size, NUM_POINT, NUM_CHANNEL))
     batch_one_hot_to_feed = np.zeros((batch_size, NUM_CLASS))
     sess, ops = get_model(batch_size=batch_size, num_point=NUM_POINT)
-    correct_cnt = 0
-    for batch_idx in range(num_batches):
-        pc_list, onehot_list = get_pc_data(1 + batch_idx)
+    filename_val = os.path.join(SUNRGBD_DIR, "val_data_idx.txt")
+    val_list = [int(line.rstrip()) for line in open(filename_val)]
+
+    for j in range(len(val_list)):
+        batch_idx = val_list[j]
+        print ("----------------------------------------------\n")
+        print ("Image:", batch_idx)
+        filename = os.path.join(SAVE_DIR, '%06d.jpg' % (batch_idx))
+        if os.path.isfile(filename):
+            img = cv2.imread(filename)
+        else:
+            img_filename = os.path.join(IMG_DIR, '%06d.jpg' % (batch_idx))
+            img = cv2.imread(img_filename)
+
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        pc_list, onehot_list, rot_angle_list, type_list, box2d_list,  calib = get_batch(batch_idx)
         for i in range(len(pc_list)):
             batch_data = pc_list[i]
             one_hot_vec = onehot_list[i]
+            rot_angle = rot_angle_list[i]
+            box2d = box2d_list[i]
             batch_data_to_feed[0:cur_batch_size, ...] = batch_data
             batch_one_hot_to_feed[0:cur_batch_size, :] = one_hot_vec
             batch_output, batch_center_pred, batch_hclass_pred, batch_hres_pred, batch_sclass_pred, batch_sres_pred, batch_scores = inference(
                 sess, ops, batch_data_to_feed, batch_one_hot_to_feed, batch_size=batch_size)
-            print(batch_hclass_pred.shape, batch_hres_pred.shape)
-            print(batch_sclass_pred.shape, batch_sres_pred.shape)
+            # Option 1:
+            heading_angle = class2angle(batch_hclass_pred[0], batch_hres_pred[0], NUM_HEADING_BIN) + rot_angle
+            box_size = class2size(batch_sclass_pred[0], batch_sres_pred[0])  # l,w,h
+            center= rotate_pc_along_y(np.expand_dims(batch_center_pred[0], 0), -rot_angle).squeeze()
+            corners_3d = get_3d_box(box_size, heading_angle, center)
+            corners_upright_depth = calib.project_upright_camera_to_upright_depth(corners_3d)
+            box3d_pts_2d, _ = calib.project_upright_depth_to_image(corners_upright_depth)
+
+            # # Option 2:
+            # heading_angle = class2angle(batch_hclass_pred[0], batch_hres_pred[0], NUM_HEADING_BIN)
+            # box_size = class2size(batch_sclass_pred[0], batch_sres_pred[0])  # l,w,h
+            # center = batch_center_pred[0]
+            # corners_3d = get_3d_box(box_size, heading_angle, center)
+            # corners_3d = rotate_pc_along_y(corners_3d, -rot_angle)
+            # center2 = [np.average(corners_3d[:, 0]), np.average(corners_3d[:, 1]), np.average(corners_3d[:,2])]
+            # corners_upright_depth = calib.project_upright_camera_to_upright_depth(corners_3d)
+            # box3d_pts_2d, _ = calib.project_upright_depth_to_image(corners_upright_depth)
+
+            img = draw_projected_box3d(img, box3d_pts_2d, color=COLORS[type_list[i]])
+            img = draw_label(img, box3d_pts_2d[0, 0], box3d_pts_2d[0, 1], type_list[i], COLORS[type_list[i]])
+        cv2.imwrite(filename, img)
 
 
 if __name__ == '__main__':
-    test_pointcloud()
+    test_data()
