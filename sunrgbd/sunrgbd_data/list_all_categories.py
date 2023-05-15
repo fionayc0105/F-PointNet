519d0459d7ff9890e75135366c7d36537b34d31a
fiona_hung <fionayc0105@gmail.com>
2023-05-15 15:33:45 +0800

    1. 顯示sample個數大於200的項目 2.對項目名稱進行sorting

--- sunrgbd/sunrgbd_data/list_all_categories.py
+++ sunrgbd/sunrgbd_data/list_all_categories.py
@@ -1,4 +1,7 @@
 import os
+
+import numpy as np
+
 from sunrgbd_data import sunrgbd_object
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -15,14 +18,25 @@ def save(filename, contents):
 
 def list_all():
     cate_list = []
+    counts = {}
     for data_idx in range(1, 10335):
         objects = SUNRGBD_DARASET.get_label_objects(data_idx)
         for obj_idx in range(len(objects)):
             obj = objects[obj_idx]
+            if counts.get(obj.classname) == None:
+                counts[obj.classname] = 1
+            else:
+                counts[obj.classname] += 1
             if obj.classname not in cate_list:
                 cate_list.append(obj.classname)
 
-    content = ''.join(['%s' % (x) + "\n" for x in cate_list])
+    cate_list = np.sort(cate_list)
+
+    content = ''
+    for x in cate_list:
+        if counts[x] > 200:
+            content += '%s %d'%(x, counts[x]) + "\n"
+    # content = ''.join(['%s %d'%(x, counts[x]) + "\n" for x in cate_list])
     filename = os.path.join(SUNRGBD_DIR, "categories.txt")
     save(filename, content)
 
